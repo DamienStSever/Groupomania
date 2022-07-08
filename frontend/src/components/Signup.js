5faf58417f647712b9cc4af4e01c782ba23ddfc3
Damien Saint-sever <damiensaintsever@hotmail.fr>
2022-07-08 15:23:20 +0200

    dernier Frontend

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -32,42 +32,52 @@ function Signup() {
   const [pseudo, setPseudo] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [description, setDescription]= useState("")
-
+  const [description, setDescription] = useState("")
+  
   // Fait apparraitre le bouton si l utilisateur n est pas connecté
   const [user, setUser] = useState(0)
   useEffect(() => {
 
-      var signup = sessionStorage.getItem("id")
-      if (signup == null) {
-          signup = " "
+    var signup = sessionStorage.getItem("id")
+    if (signup == null) {
+      signup = " "
 
 
-      } else {
-          signup = null
-         
-      }
-      
-      setUser(signup)
+    } else {
+      signup = null
+
+    }
+
+    setUser(signup)
 
   }, []);
 
-  const  signup  = () => {
+  const signup = () => {
     Axios.post("http://localhost:4200/api/user/signup", {
       pseudo: pseudo,
       email: email,
       password: password,
-      description:description
+      description: description
     }).then((res) => {
-      console.log(res);
-        })
-        
-
+      
+    })
   };
-  
+ function emailValid(email, password){
+  const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  const passwordRegex = /^[A-Za-z\d]{6,}$/;
+  if (!email.match(emailRegex)){
+    alert("Erreur: votre email est invalide")
+  } else if(!password.match(passwordRegex)){
+    alert("Mot de passe trop court ou mauvais caractères utilisés \n 6 caractères minimum (Lettres ou numéros)")
+  } else{
+  signup()
+  window.location.reload()
+  alert("Compte crée veuillez vous pouvez vous connecter à présent")
+  }
+ }
   return (
     <div>
-      {user &&<button onClick={openModal} className="userbutton">S'inscrire</button>}
+      {user && <button onClick={openModal} className="userbutton">S'inscrire</button>}
       <Modal
         isOpen={modalIsOpen}
         onAfterOpen={afterOpenModal}
@@ -80,32 +90,32 @@ function Signup() {
         <div className="form">pseudo</div>
         <form>
           <input id="inputPseudo" onChange={(e) => {
-                        setPseudo(e.target.value)
-                    }} />
+            setPseudo(e.target.value)
+          }} />
         </form>
         <div className="form">email</div>
         <form>
           <input id="inputEmail" onChange={(e) => {
-                        setEmail(e.target.value)
-                    }} />
+            setEmail(e.target.value)
+          }} />
         </form>
-        <div className="form">Password
-        <form>
-          <input id="inputPassword" type="password" onChange={(e) => {
-                        setPassword(e.target.value)
-                    }} /> <br />
-                    
-                    
-        </form>
+        <div className="form">Password ( 6 lettres ou caractères minimum)
+          <form>
+            <input id="inputPassword" type="password" onChange={(e) => {
+              setPassword(e.target.value)
+            }} /> <br />
+
+
+          </form>
         </div>
         <div className="form"> Description
           <form>
-          <textarea id="inputDescription"  onChange={(e) => {
-                        setDescription(e.target.value)
-                    }} />
+            <textarea id="inputDescription" onChange={(e) => {
+              setDescription(e.target.value)
+            }} />
           </form>
         </div>
-        <button onClick={signup}>Valider</button>
+        <button onClick={(() => signup, () => emailValid(email, password)) }>Valider</button>
       </Modal>
     </div>
 
